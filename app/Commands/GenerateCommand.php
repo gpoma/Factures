3a2f0a2ea8240eab17519a86e008ac9351d5a855
Gabriel Poma <gpoma@24eme.fr>
2019-08-16 16:36:13 +0200

    Selection des fichiers pour la génération

--- app/Commands/GenerateCommand.php
+++ app/Commands/GenerateCommand.php
@@ -4,15 +4,26 @@ namespace App\Commands;
 
 use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
+use League\Csv\Reader;
+use PhpOffice\PhpSpreadsheet\IOFactory;
 
 class GenerateCommand extends Command
 {
+    /**
+     * Le dossier où sera stocké les fichiers csv
+     *
+     * @var string
+     */
+    protected $tmp = __DIR__.'/../../tmp';
+
     /**
      * The signature of the command.
      *
      * @var string
      */
-    protected $signature = 'generate {periode : La période à générer}';
+    protected $signature = 'generate
+                            {periode : La période à générer}
+                            {--name=all : Le fichier à générer}';
 
     /**
      * The description of the command.
@@ -28,7 +39,27 @@ class GenerateCommand extends Command
      */
     public function handle()
     {
-        //
+        $files = [];
+        $periode = $this->argument('periode');
+        $to_generate = $this->option('name');
+        $saved_directory = implode(DIRECTORY_SEPARATOR, [$this->tmp, $periode, '']);
+
+        if ($to_generate === 'all') {
+            $files = glob($saved_directory . '*.csv');
+        } else {
+            foreach (explode(',', $to_generate) as $name) {
+                $files[] = $saved_directory . $name . '.csv';
+            }
+        }
+
+        foreach ($files as $file) {
+            if (! file_exists($file)) {
+                $this->error(basename($file, '.csv') . ' does not exists');
+                continue;
+            }
+
+            $reader = Reader::createFromPath($file, 'r');
+        }
     }
 
     /**
